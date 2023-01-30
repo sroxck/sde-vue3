85f27116c4274b3585657eec8c43292ad36e78b4
sroxck <642261686@qq.com>
2023-01-30 22:41:13 +0800

    feat: init sde for support vue3

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2023-01-30 20:50:55
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2023-01-30 22:19:17
+ * @FilePath: \emr-editor\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/data': {
+        target: 'http://localhost:5173/static/sdeEditor/',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          // proxy 是 'http-proxy' 的实例
+        }
+      },
+    },
+  },
+})
